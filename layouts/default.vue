8f44a2c7cfb8df1553cc5fc1113ff60acd1f747f
NMTuan <nmtuan@qq.com>
2022-09-14 16:23:25 +0800

    chore: 最小功能完成.

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,18 +2,14 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-07-29 16:05:46
- * @LastEditTime: 2022-09-14 09:05:40
+ * @LastEditTime: 2022-09-14 16:06:37
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \ezMusic\layouts\default.vue
 -->
 <template>
     <div class="flex flex-col justify-center h-full">
-        <MyPlayerTest />
-        <div>
-            <NuxtLink :to="{name: 'index'}">[index]</NuxtLink>
-            <NuxtLink :to="{name: 'playlist'}">[playList]</NuxtLink>
-        </div>
+        <MyPlayer />
         <slot />
     </div>
 </template>
